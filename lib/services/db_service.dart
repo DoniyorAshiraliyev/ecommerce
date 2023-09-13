62f7ff90a18718acd6004252b1ce69e90c6e5809
abduraimovdev <kenzo778118@gmail.com>
2023-09-13 15:05:05 +0500

    bug

--- lib/services/db_service.dart
+++ lib/services/db_service.dart
@@ -1,77 +1,85 @@
-import 'package:ecommerce/domain/models/favourites.dart';
-import 'package:ecommerce/domain/models/product.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-// abstract class DatabaseHelper {
-//   Future<void> init();
-//
-//   Future<List<Map<String, Object?>>> readAll();
-//
-//   Future<Map<String, Object?>> readOne(String id);
-//
-//   Future<void> create(Favourites favourites );
-//
-//   Future<void> update(Map<String, Object?> item, String id);
-//
-//   Future<void> delete(String id);
-// }
-
-class SqlDatabase {
-  static final dbName = 'favourites_database.db';
+sealed class SqlDatabase {
+  static const dbName = 'words_database.db';
   static late final Future<Database> database;
 
-   static Future<void> init() async {
+  static Future<void> init() async {
     String pathDirectory = await getDatabasesPath();
     String path = join(pathDirectory, dbName);
 
     database = openDatabase(path, onCreate: _onCreate, version: 1);
   }
 
-  static Future<void> _onCreate(Database db, int version) {
-    return db.execute(
-      'CREATE TABLE products(id INTEGER PRIMARY KEY, title TEXT, price NUMERIC, description TEXT, category TEXT, image TEXT, ratingMap TEXT)',
+  static Future<void> _onCreate(Database db, int version) async{
+    await db.execute(
+      'CREATE TABLE favourite(id INTEGER PRIMARY KEY, product TEXT)',
     );
+    // await db.execute(
+    //   'CREATE TABLE orders(id INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT, translation TEXT, isMemorized int)',
+    // );
+
   }
 
-  Future<void> insertProduct(ProductModel product) async {
+  static Future<void> insert({required SqlTable table, required Map<String, Object?> data, }) async {
     final db = await database;
+
     await db.insert(
-      'products',
-      product.toJson(),
+      table.name,
+      data,
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 
-  Future<List<ProductModel>> getAllProducts() async {
+  static Future<void> update({required SqlTable table, required Map<String, Object?> data, required int id}) async {
     final db = await database;
-    final result = await db.query('products');
-    final products = <ProductModel>[];
-    for (final map in result) {
-      final ratingMap = map['ratingMap'];
-      final rating = RatingModel.fromJson(ratingMap as Map<String,dynamic>);
-      final product = ProductModel.fromJson(map,rating);
-      products.add(product);
-    }
-    return products;
-  }
 
-  Future<void> updateProduct(ProductModel product) async {
-    final db = await database;
     await db.update(
-      'products',
-      product.toJson(),
-      where: 'id = ?',
-      whereArgs: [product.id],
-    );
-  }
-
-  Future<void> deleteProduct(int id) async {
-    final db = await database;
-    await db.delete(
-      'products',
+      table.name,
+      data,
       where: 'id = ?',
       whereArgs: [id],
     );
   }
+
+  static Future<List<Map<String, dynamic>>> readAll({required SqlTable table}) async {
+    final db = await database;
+
+    return await db.query(table.name);
+
+  }
+
+  static Future<void> delete({required SqlTable table, required int id}) async {
+    final db = await database;
+
+    await db.delete(
+      table.name,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  static Future<Map<String, dynamic>> read({required SqlTable table, required String id}) async {
+    final db = await database;
+
+    List<Map<String, Object?>> query = await db.query(
+      table.name,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    return query[0];
+  }
+
+  static Future<void> deleteAll({required SqlTable table}) async{
+    final db = await database;
+
+    await db.execute("DELETE FROM ${table.name}");
+  }
+}
+
+
+enum SqlTable {
+  favourite,
+  orders,
 }
